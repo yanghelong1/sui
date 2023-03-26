1aac36e343e9ad3bec5438372ac372051293d013
Ashok Menon <ashok@mystenlabs.com>
2023-03-26 14:55:38 +0100

    [move-package] MovePackage accepts CompiledModules by reference
    Preparation for package upgrades, where to verify `CompiledModules`,
    we need linkage and type origin tables, which are created as part of a
    `MovePackage`, but currently, the `MovePackage` is created after
    verification.
    
    This change allows us to deserialize bytes to create
    `CompiledModule`s, then build a `MovePackage` from those, and use that
    to verify the deserialized modules.
    
    Test Plan:
    
    ```
    $ cargo simtest
    $ env SUI_SKIP_SIMTESTS=1 cargo nextest run
    ```

--- crates/sui-types/src/unit_tests/base_types_tests.rs
+++ crates/sui-types/src/unit_tests/base_types_tests.rs
@@ -15,7 +15,6 @@ use crate::crypto::{
     get_key_pair, get_key_pair_from_bytes, AccountKeyPair, AuthorityKeyPair, AuthoritySignature,
     Signature, SuiAuthoritySignature, SuiSignature,
 };
-use crate::OBJECT_START_VERSION;
 use crate::{gas_coin::GasCoin, object::Object, SUI_FRAMEWORK_ADDRESS};
 use shared_crypto::intent::{Intent, IntentMessage, IntentScope};
 use sui_protocol_config::ProtocolConfig;
@@ -371,8 +370,7 @@ fn test_move_object_size_for_gas_metering() {
 fn test_move_package_size_for_gas_metering() {
     let module = file_format::empty_module();
     let package = Object::new_package(
-        vec![module],
-        OBJECT_START_VERSION,
+        &[module],
         TransactionDigest::genesis(),
         ProtocolConfig::get_for_max_version().max_move_package_size(),
         &[], // empty dependencies for empty package (no modules)
